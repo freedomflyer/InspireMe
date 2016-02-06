7301cd4d767b2c7be3163608c3d2301c45404df4
Spencer Gardner <sgardner@getartemis.com>
2016-02-06 16:17:52 -0700

    response func

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(serveStatic(__dirname + '/public'));
 // Routes
 
 app.get('/', routes.index);
+app.get('/quote', routes.quote);
 
 // load errorHandler after routes
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 function getClientIp(req) {
-  
+
   var ipAddress;
   // Amazon EC2 / Heroku workaround to get real client IP
-  var forwardedIpsStr = req.header('x-forwarded-for'); 
+  var forwardedIpsStr = req.header('x-forwarded-for');
   if (forwardedIpsStr) {
-    
+
     // 'x-forwarded-for' header may return multiple IP addresses in
     // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
     // the first one
@@ -24,5 +24,23 @@ function getClientIp(req) {
  */
 
 exports.index = function(req, res) {
-  res.end('Hello World');  
+  res.end('Hello World');
 };
+
+exports.quote = function(req, res) {
+    res.send({
+        "response": {
+            "outputSpeech": {
+            "type": "PlainText",
+            "text": "Ok, What's the topic for this event?"
+            },
+            "shouldEndSession": false
+        },
+        "sessionAttributes": {
+            "intentSequence": "createEvent;duration;yes",
+            "forDate": "2015-06-04",
+            "startTime": "14:00",
+            "duration": "PT45M",
+        }
+    });
+}
